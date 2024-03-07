01b01a7124f6b11f99c9b2f444c4b08b61f96664
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2024-03-07 10:27:36 -0300

    Initial Commit

---
+++ MyBooksGo/MyBooksGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksGoApp.swift
+//  MyBooksGo
+//
+//  Created by Thiago Menezes on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
