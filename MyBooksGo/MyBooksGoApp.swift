6e8028efe3ac8f21300a98110af665192953b4a5
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2024-03-11 10:07:47 -0300

    formas de setar configuração do ModelContainer

--- MyBooksGo/MyBooksGoApp.swift
+++ MyBooksGo/MyBooksGoApp.swift
@@ -10,12 +10,41 @@ import SwiftData
 
 @main
 struct MyBooksGoApp: App {
+    
+    ///Container do projeto
+    let container: ModelContainer
+    
     var body: some Scene {
         WindowGroup {
             BookListView()
         }
-        .modelContainer(for: [Book.self])
+        .modelContainer(for: Book.self)
     }
     
-
+    init() {
+        
+        let schema = Schema([Book.self])
+        let config = ModelConfiguration("MyBooks", schema: schema)
+        
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Could not configure the container")
+        }
+        
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
+        
+        //N faço ideia do porque destes parametros em URL
+        //Aparentemente configura o local em que o ModelContianer será criado
+//        let config = ModelConfiguration(url: URL.documentsDirectory.appending(path: "MyBooks.store"))
+//        
+//        do {
+//            //instância container
+//            container = try ModelContainer(for: Book.self, configurations: config)
+//        } catch {
+//            fatalError("Could not configure the container")
+//        }
+        
+//        print(URL.documentsDirectory.path())
+    }
 }
